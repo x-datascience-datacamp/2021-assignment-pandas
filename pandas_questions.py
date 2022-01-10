d9eceb1780e0046818fb4587429576102f51d9a0
Louis Popi <theh2o64@gmail.com>
2022-01-10 20:50:40 +0100

    2021-assignment-pandas: Remove useless code

--- pandas_questions.py
+++ pandas_questions.py
@@ -13,22 +13,6 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 
 
-# DOM-TOM-COM
-dom_tom_com = {
-    'WALLIS-ET-FUTUNA',
-    'SAINT PIERRE ET MIQUELON',
-    'SAINT-MARTIN/SAINT-BARTHELEMY',
-    "FRANCAIS DE L'ETRANGER",
-    'MAYOTTE',
-    'POLYNESIE FRANCAISE',
-    'LA REUNION',
-    'GUYANE',
-    'GUADELOUPE',
-    'MARTINIQUE',
-    'NOUVELLE CALEDONIE'
-}
-
-
 def load_data():
     """Load data from the CSV files referundum/regions/departments."""
     referendum = pd.read_csv("data/referendum.csv", sep=';')
@@ -79,7 +63,7 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     ret = pd.merge(referendum, regions_and_departments,
                    left_on='Department code', right_on='code_dep', how='left')
     # Drop lines with DOM-TOM-COM
-    ret = ret[~ret['Department name'].isin(dom_tom_com)]
+    ret = ret[ret['Department name'] != "FRANCAIS DE L'ETRANGER"]
     # Drop rows with missing values
     ret = ret.dropna()
     return ret
@@ -91,16 +75,13 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-    # Build a mapping from code_reg to name_reg
-    # NOTE: A warning tells me it's bad but it shouldn't
-    m = referendum_and_areas[['code_reg', 'name_reg']] \
-        .set_index('code_reg').T.to_dict()
     # Select the columns and do a group by
     cols = ['name_reg', 'code_reg', 'Registered',
             'Abstentions', 'Null', 'Choice A', 'Choice B']
-    ret = referendum_and_areas[cols].groupby(['code_reg']).sum()
-    # Add the name_reg
-    ret['name_reg'] = ret.index.map(lambda x: m[x]['name_reg'])
+    ret = referendum_and_areas[cols].groupby(['code_reg', 'name_reg']).sum()
+    # Change the index
+    ret = ret.reset_index()
+    ret = ret.set_index('code_reg')
     return ret
 
 
@@ -115,8 +96,6 @@ def plot_referendum_map(referendum_result_by_regions):
     """
     # Load the geopandas
     regions_geo = gpd.read_file('data/regions.geojson')
-    # Remove DOM-TOM-COM
-    regions_geo = regions_geo[regions_geo['code'].apply(int) > 6]
     # Compute the ratios
     referendum_result_by_regions['ratio'] = \
         referendum_result_by_regions['Choice A'] \
@@ -125,11 +104,13 @@ def plot_referendum_map(referendum_result_by_regions):
     # Merge with referendum_result_by_regions
     ret = pd.merge(referendum_result_by_regions, regions_geo,
                    left_on='code_reg', right_on='code')
+    # Drop the DOM-TOM-COM
+    ret = ret.dropna()
     # Build the output by selection columns
     output = gpd.GeoDataFrame(ret[['ratio', 'name_reg']
                                   + list(regions_geo.columns)])
     # Plot everything
-    output.plot('ratio')
+    output.plot('ratio', legend=True)
     return output
 
 
