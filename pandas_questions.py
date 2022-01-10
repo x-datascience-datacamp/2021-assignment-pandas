8dd1419f5c8396ee008e13208ee0d3c3eedd9f65
RonnyTZ <ronny_75@hotmail.com>
2022-01-10 22:23:22 +0100

    Solution pandas check docstyle version 2

--- pandas_questions.py
+++ pandas_questions.py
@@ -23,10 +23,8 @@ def load_data():
 
 def merge_regions_and_departments(regions, departments):
     """Merge regions and departments in one DataFrame.
-    
     The columns in the final DataFrame should be:
-    ['code_reg', 'name_reg', 'code_dep', 'name_dep']
-    """
+        ['code_reg', 'name_reg', 'code_dep', 'name_dep']"""
     # Merge both dataframes
     df_merge = pd.merge(regions, departments, left_on='code',
                         right_on='region_code')
@@ -39,10 +37,8 @@ def merge_regions_and_departments(regions, departments):
 
 def merge_referendum_and_areas(referendum, regions_and_departments):
     """Merge referendum and regions_and_departments in one DataFrame.
-    
     You can drop the lines relative to DOM-TOM-COM departments, and the
-    french living abroad.
-    """
+    french living abroad."""
     referendum['Department code'] = referendum['Department code'].str.zfill(2)
     # Merge both dataframes
     df_merge = pd.merge(referendum, regions_and_departments,
@@ -54,7 +50,6 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
 
 def compute_referendum_result_by_regions(referendum_and_areas):
     """Return a table with the absolute count for each region.
-    
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
@@ -66,7 +61,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
 
 def plot_referendum_map(referendum_result_by_regions):
     """Plot a map with the results from the referendum.
-    
     * Load the geographic data with geopandas from `regions.geojson`.
     * Merge these info into `referendum_result_by_regions`.
     * Use the method `GeoDataFrame.plot` to display the result map. The results
