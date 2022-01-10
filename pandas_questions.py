b1f8708435c8233e8ebe690a482c1c1ac41f38b2
Louis Popi <theh2o64@gmail.com>
2022-01-10 18:17:24 +0100

    2021-assignment-pandas: Fix pydocstyle issues D202/103

--- pandas_questions.py
+++ pandas_questions.py
@@ -43,7 +43,6 @@ def merge_regions_and_departments(regions, departments):
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
-
     # Merge both
     ret = pd.merge(regions, departments, left_on='code', right_on='region_code')
     # Select columns
@@ -53,7 +52,10 @@ def merge_regions_and_departments(regions, departments):
     return ret
 
 def normalize_refendum(dep_code):
-    # dep_code should be two digits
+    """Normalize department code on refendum file.
+
+    Return a well-formated department code.
+    """
     if len(dep_code) == 1: return '0' + dep_code
     else: return dep_code
 
@@ -63,7 +65,6 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-
     # Clean referendum
     # NOTE: DOM-TOM-COM are encoded in Z. format but we won't care as we'll drop it
     referendum['Department code'] = referendum['Department code'].apply(normalize_refendum)
@@ -81,7 +82,6 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-
     # Build a mapping from code_reg to name_reg
     # NOTE: A warning tells me it's bad but it shouldn't
     m = referendum_and_areas[['code_reg','name_reg']].set_index('code_reg').T.to_dict()
@@ -102,9 +102,9 @@ def plot_referendum_map(referendum_result_by_regions):
       should display the rate of 'Choice A' over all expressed ballots.
     * Return a gpd.GeoDataFrame with a column 'ratio' containing the results.
     """
-
     # Load the geopandas
     regions_geo = gpd.read_file('data/regions.geojson')
+    print(regions_geo)
     # Remove DOM-TOM-COM
     regions_geo = regions_geo[regions_geo['code'].apply(int) > 6]
     # Compute the ratios
