59fb482df1c334948546c2b252497c1f7afcc5be
huan <huan.macbook.com>
2022-01-13 11:56:39 +0100

    debugged

--- pandas_questions.py
+++ pandas_questions.py
@@ -24,6 +24,7 @@ def load_data():
 
 def merge_regions_and_departments(regions, departments):
     """Merge regions and departments in one DataFrame.
+
     The columns in the final DataFrame should be:
     ['code_reg', 'name_reg', 'code_dep', 'name_dep']
     """
@@ -41,18 +42,12 @@ def merge_referendum_and_areas(referendum, regions_and_departments):
     You can drop the lines relative to DOM-TOM-COM departments, and the
     french living abroad.
     """
-    ref = referendum.rename(
-        columns={'Department code': 'code_dep'}).drop(
-        columns=['Department name'])
 
-    def format(x):
-        if len(x) == 1:
-            return "0" + x
-        else:
-            return x
-
-    ref['code_dep'] = ref['code_dep'].apply(format)
-    return pd.merge(ref, regions_and_departments, on='code_dep', how='left')
+    referendum['code_dep'] = referendum['Department code'].str.zfill(2)
+    ans = pd.merge(referendum, regions_and_departments,
+                   on='code_dep', how='left')
+    ans = ans.dropna()
+    return ans
 
 
 def compute_referendum_result_by_regions(referendum_and_areas):
@@ -61,11 +56,10 @@ def compute_referendum_result_by_regions(referendum_and_areas):
     The return DataFrame should be indexed by `code_reg` and have columns:
     ['name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A', 'Choice B']
     """
-    referendum_result_by_regions = referendum_and_areas[
-        ['code_reg',
-         'name_reg', 'Registered', 'Abstentions', 'Null', 'Choice A',
-         'Choice B']].groupby(['code_reg', 'name_reg']).sum()
-    return referendum_result_by_regions
+    ans = referendum_and_areas.groupby(
+        ['code_reg', 'name_reg']).sum().reset_index().set_index('code_reg')
+    ans = ans.drop(columns="Town code")
+    return ans
 
 
 def plot_referendum_map(referendum_result_by_regions):
@@ -81,11 +75,10 @@ def plot_referendum_map(referendum_result_by_regions):
         df = gpd.read_file(france)
         df.rename(columns={'code': 'code_reg'}, inplace=True)
         df.set_index('code_reg', inplace=True)
-        out = pd.merge(df, referendum_result_by_regions, on='code_reg')
-        out['ratio'] = out['Choice A'] / (out['Choice A'] +
-                                          out['Choice B'] + out['Null'])
-        out.plot('ratio', legend=True)
-    return out
+        ans = pd.merge(df, referendum_result_by_regions, on='code_reg')
+        ans['ratio'] = ans['Choice A'] / (ans['Choice A'] + ans['Choice B'])
+        ans.plot('ratio', legend=True)
+    return ans.reset_index()
 
 
 if __name__ == "__main__":
